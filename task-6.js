caa3758ec7f71d1401e4a5dc6c08b2dbb70542a2
hngovan <hngovan1999@gmail.com>
2024-04-26 17:20:20 +0700

    update swap task 6

--- task-6.js
+++ task-6.js
@@ -1,3 +1,9 @@
 export default function () { 
   console.log("task 6!") 
+  // swap value a, b not using tmp
+  let a = 6;
+  let b = 4;
+  a = a - b; // a = 6 - 4 = 2
+  b = a + b; // b = 2 + 4 = 6
+  a = b - a; // a = 6 - 2 = 4
 }
