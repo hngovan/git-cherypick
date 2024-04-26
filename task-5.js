ab0470165cecbe35850167b0c0f3844237eb0b72
hngovan <hngovan1999@gmail.com>
2024-04-26 15:23:59 +0700

    update task 5

--- task-5.js
+++ task-5.js
@@ -1,3 +1,19 @@
 export default function () { 
-  console.log("task 5.1!") 
+  console.log("task 5.1!")
+
+  class Chameleon {
+    static colorChange(newColor) {
+      this.newColor = newColor;
+      return this.newColor;
+    }
+  
+    constructor({ newColor = "green" } = {
+      
+    }) {
+      this.newColor = newColor;
+    }
+  }
+  
+  const freddie = new Chameleon({ newColor: "purple" });
+  freddie.colorChange("orange");
 }
